79ba02694eae2967cbf286a19ab8af371f1cd609
EyuaelB <eyuaelbezabeh@gmail.com>
2024-07-17 12:45:48 +0300

    time function fix

--- services/auth_service.py
+++ services/auth_service.py
@@ -4,7 +4,7 @@ from schemas import UserCreate, Token
 from passlib.context import CryptContext
 from jose import JWTError, jwt
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -20,15 +20,15 @@ REFRESH_TOKEN_EXPIRE_MINUTES = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES"))
 def create_access_token(email: str, expires_delta: timedelta = None):
     to_encode = {"sub": email}
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
 
 def create_refresh_token(email: str):
     to_encode = {"sub": email}
-    expire = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, REFRESH_SECRET_KEY, algorithm="HS256")
 
